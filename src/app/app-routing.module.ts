5cc22cd0c37583dd02dd828c28c7f4d87b3c192e
Sunny Tong <50933364+SunnyTong2019@users.noreply.github.com>
2019-12-23 22:48:23 -0800

    set up routing for search and saved books components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { SaveComponent } from './save/save.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SearchComponent },
+  { path: 'saved', component: SaveComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { SearchComponent } from './search/search.component';
+import { SaveComponent } from './save/save.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    JumbotronComponent
+    JumbotronComponent,
+    SearchComponent,
+    SaveComponent
   ],
   imports: [
     BrowserModule,
