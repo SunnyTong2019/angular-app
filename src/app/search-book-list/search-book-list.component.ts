7388acca93099d19782f057c58b56640ab36b0b6
Sunny Tong <50933364+SunnyTong2019@users.noreply.github.com>
2019-12-24 23:29:14 -0800

    displayed book list from search results data

--- src/app/search-book-list/search-book-list.component.ts
+++ src/app/search-book-list/search-book-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-search-book-list',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-book-list.component.css']
 })
 export class SearchBookListComponent implements OnInit {
-
+   
+  @Input() bookList:object[];
   constructor() { }
 
   ngOnInit() {
